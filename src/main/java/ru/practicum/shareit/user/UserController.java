6c1b0eb45d8944678a58d391d2e736056f21a91e
P.Sergey <pavbatol@yandex.ru>
2022-11-19 16:28:24 +0400

    feat: Добавить абстракт для контроллеров

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -2,12 +2,21 @@ package ru.practicum.shareit.user;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.shareit.common.impl.AbstractController;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.service.UserService;
 
 /**
  * TODO Sprint add-controllers.
  */
 @RestController
 @RequestMapping(path = "/users")
-public class UserController {
+public class UserController extends AbstractController<User, UserService> {
 
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        super(userService);
+        this.userService = userService;
+    }
 }
