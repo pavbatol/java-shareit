e01db5d4be0fd4b506e5598b40dc7af182fec78a
P.Sergey <pavbatol@yandex.ru>
2022-12-04 17:28:23 +0400

    refactor: Correct entities to JPA

--- src/main/java/ru/practicum/shareit/booking/model/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -1,35 +1,62 @@
 package ru.practicum.shareit.booking.model;
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.Hibernate;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
-import java.time.LocalDate;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 /**
  * TODO Sprint add-bookings.
  */
 
-@Data
-@Builder
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "bookings")
 public class Booking {
 
-    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "booking_id")
     Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "booker_id", nullable = false)
     User booker;
 
+    @ManyToOne
+    @JoinColumn(name = "item_id", nullable = false)
     Item item;
 
-    LocalDate start;
+    @Column(name = "start_time", nullable = false)
+    LocalDateTime start;
 
-    LocalDate end;
+    @Column(name = "end_time", nullable = false)
+    LocalDateTime end;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 50)
     BookingStatus status;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Booking booking = (Booking) o;
+        return id != null && Objects.equals(id, booking.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
