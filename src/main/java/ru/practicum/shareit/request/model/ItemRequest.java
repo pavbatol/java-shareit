364092ca17c88e7869de9748751e1b4c853f60e9
P.Sergey <pavbatol@yandex.ru>
2022-12-05 13:22:30 +0400

    fix: Correct and add ItemRequest to schema.sql

--- src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
@@ -1,32 +1,53 @@
 package ru.practicum.shareit.request.model;
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.Hibernate;
 import ru.practicum.shareit.user.model.User;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 /**
  * TODO Sprint add-item-requests.
  */
 
-@Data
-@Builder
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "requests")
 public class ItemRequest {
 
-    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "request_id")
     Long id;
 
-    String name;
-
+    @Column(name = "req_description", nullable = false)
     String description;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
     User requester;
 
+    @Column(nullable = false)
     LocalDateTime created;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        ItemRequest that = (ItemRequest) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
