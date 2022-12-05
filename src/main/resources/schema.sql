ae84f90ef61e24d025e872594e75ccb51750f71b
P.Sergey <pavbatol@yandex.ru>
2022-12-05 15:10:57 +0400

    fix: Correct application-h2, application-ps, schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS items
     request_id       BIGINT,
     owner_id         BIGINT                                  NOT NULL,
     CONSTRAINT pk_items PRIMARY KEY (item_id),
-    CONSTRAINT fk_items_owner_id_to_users FOREIGN KEY (owner_id) REFERENCES users (user_id)
+    CONSTRAINT fk_items_owner_id_to_users FOREIGN KEY (owner_id) REFERENCES users (user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS bookings
@@ -61,8 +61,8 @@ CREATE TABLE IF NOT EXISTS bookings
     end_time   TIMESTAMP WITHOUT TIME ZONE             NOT NULL,
     status     VARCHAR(50)                             NOT NULL,
     CONSTRAINT pk_bookings PRIMARY KEY (booking_id),
-    CONSTRAINT fk_bookings_booker_id_to_users FOREIGN KEY (booker_id) REFERENCES users(user_id),
-    CONSTRAINT fk_bookings_item_id_to_items FOREIGN KEY (item_id) REFERENCES items(item_id)
+    CONSTRAINT fk_bookings_booker_id_to_users FOREIGN KEY (booker_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    CONSTRAINT fk_bookings_item_id_to_items FOREIGN KEY (item_id) REFERENCES items(item_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS requests
@@ -72,7 +72,7 @@ CREATE TABLE IF NOT EXISTS requests
     requester_id    BIGINT                                  NOT NULL,
     created         TIMESTAMP WITHOUT TIME ZONE             NOT NULL,
     CONSTRAINT pk_requests PRIMARY KEY (request_id),
-    CONSTRAINT fk_requester_id_to_users FOREIGN KEY (requester_id) REFERENCES users (user_id)
+    CONSTRAINT fk_requester_id_to_users FOREIGN KEY (requester_id) REFERENCES users (user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS comments
@@ -82,7 +82,7 @@ CREATE TABLE IF NOT EXISTS comments
     item_id    BIGINT                                  NOT NULL,
     author_id  BIGINT                                  NOT NULL,
     CONSTRAINT pk_comments PRIMARY KEY (comment_id),
-    CONSTRAINT fk_comments_item_id_to_items FOREIGN KEY (item_id) REFERENCES items (item_id),
-    CONSTRAINT fk_comments_author_id_to_users FOREIGN KEY (author_id) REFERENCES users (user_id)
+    CONSTRAINT fk_comments_item_id_to_items FOREIGN KEY (item_id) REFERENCES items (item_id) ON DELETE CASCADE,
+    CONSTRAINT fk_comments_author_id_to_users FOREIGN KEY (author_id) REFERENCES users (user_id) ON DELETE CASCADE
 );
 
