7e9ed636efae6f166df32d693a4d6f2ea245b428
P.Sergey <pavbatol@yandex.ru>
2022-12-06 13:46:34 +0400

    fix: Modify mappers

--- src/main/java/ru/practicum/shareit/booking/model/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/model/BookingMapper.java
@@ -2,14 +2,23 @@ package ru.practicum.shareit.booking.model;
 
 import org.mapstruct.Mapping;
 import ru.practicum.shareit.common.Mapper;
+import ru.practicum.shareit.item.model.ItemMapper;
+import ru.practicum.shareit.user.model.UserMapper;
 
-@org.mapstruct.Mapper(componentModel = "spring")
+@org.mapstruct.Mapper(componentModel = "spring",
+        uses = {UserMapper.class, ItemMapper.class})
 public interface BookingMapper extends Mapper<Booking, BookingDto> {
 
     @Mapping(target = "item.id", source = "dto.itemId")
     @Mapping(target = "booker.id", source = "bookerId")
     Booking toEntity(BookingAddDto dto, Long bookerId);
 
-//    @Mapping(target = "booker.id", source = "bookerId")
-//    Booking toEntity(BookingDto dto, Long bookerId);
+    @Mapping(target = "item", source = "dto.itemId")
+    @Mapping(target = "booker", source = "bookerId")
+    Booking toEntityFilledRelations(BookingAddDto dto, Long bookerId);
+
+    @Mapping(target = "item", source = "dto.itemId")
+    @Mapping(target = "booker.id", source = "bookerId")
+    Booking toEntityFilledItem(BookingAddDto dto, Long bookerId);
+
 }
--- src/main/java/ru/practicum/shareit/item/model/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/model/ItemMapper.java
@@ -1,11 +1,18 @@
 package ru.practicum.shareit.item.model;
 
 import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import ru.practicum.shareit.common.Mapper;
+import ru.practicum.shareit.item.storage.ItemRepository;
 import ru.practicum.shareit.user.model.User;
+
+import static ru.practicum.shareit.validator.ValidatorManager.getNonNullObject;
 
 @org.mapstruct.Mapper(componentModel = "spring")
 public abstract class ItemMapper implements Mapper<Item, ItemDto> {
+
+    @Autowired
+    protected ItemRepository itemRepository;
 
     @Mapping(target = "owner.id", source = "userId")
     public abstract Item toEntity(ItemDto itemDto, Long userId);
@@ -14,5 +21,9 @@ public abstract class ItemMapper implements Mapper<Item, ItemDto> {
         Item item = toEntity(itemDto);
         item.setOwner(owner);
         return item;
+    }
+
+    public Item toEntity(Long itemId) {
+        return getNonNullObject(itemRepository, itemId);
     }
 }
