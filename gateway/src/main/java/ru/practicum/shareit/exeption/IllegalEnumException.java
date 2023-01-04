63702611568e7ecae5f57fd42f4800ab9477ddf3
P.Sergey <pavbatol@yandex.ru>
2023-01-04 20:06:40 +0400

    feat: gateway: Add exceptions

---
+++ gateway/src/main/java/ru/practicum/shareit/exeption/IllegalEnumException.java
@@ -0,0 +1,7 @@
+package ru.practicum.shareit.exeption;
+
+public class IllegalEnumException extends RuntimeException {
+    public IllegalEnumException(String message) {
+        super(message);
+    }
+}
